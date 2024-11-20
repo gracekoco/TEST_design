97646a771e960e643e77ecb3cff96921daa3b1bb
gracekoco <kocograce455@gmail.com>
2024-11-20 23:29:24 +0000

    first commit

---
+++ style.js
@@ -0,0 +1,27 @@
+
+
+
+function normal_accueil(){
+    document.getElementByClassName('show').style.visibility="visible";
+}
+function visible_accueil(){
+    document.getElementByClassName('show').style.visibility="hidden";
+}
+function normal_projet(){
+    document.getElementById('ul-projet').style.visibility="hidden";
+}
+function visible_projet(){
+    document.getElementById('ul-projet').style.visibility="visible";
+}
+function normal_equipe(){
+    document.getElementById('ul-equipe').style.visibility="hidden";
+}
+function visible_equipe(){
+    document.getElementById('ul-equipe').style.visibility="visible";
+}
+function normal_contact(){
+    document.getElementById('ul-contact').style.visibility="hidden";
+}
+function visible_contact(){
+    document.getElementById('ul-contact').style.visibility="visible";
+}
